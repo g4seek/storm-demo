099896677bb899265bc044a7cf2efe144f76e7ec
g4seek <g4seek@gmail.com>
2015-06-16 19:02:11 +0800

    链接数据库读取配置文件

--- storm-demo-front/src/main/resources/dao.js
+++ storm-demo-front/src/main/resources/dao.js
@@ -1,16 +1,30 @@
 var ioc = {
+	// 读取配置文件
+	config : {
+		type : "org.nutz.ioc.impl.PropertiesProxy",
+		fields : {
+			paths : [ "jdbc.properties" ]
+		}
+	},
 	dao : {
 		type : "org.nutz.dao.impl.NutDao",
 		args : [ {
 			refer : "dataSource"
 		} ]
 	},
+	// 数据源
 	dataSource : {
 		type : "org.nutz.dao.impl.SimpleDataSource",
 		fields : {
-			jdbcUrl : 'jdbc:mysql://localhost:3306/storm?useUnicode=true&characterEncoding=UTF-8',
-			username : 'root',
-			password : ''
+			jdbcUrl : {
+				java : "$config.get('jdbc.url')"
+			},
+			username : {
+				java : "$config.get('jdbc.username')"
+			},
+			password : {
+				java : "$config.get('jdbc.password')"
+			}
 		}
 	}
-}
+};
