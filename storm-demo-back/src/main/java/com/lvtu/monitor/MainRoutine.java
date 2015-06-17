42c36f4746d51ff9731684ed53cdc35cc1b46bb8
g4seek <g4seek@gmail.com>
2015-06-17 15:48:07 +0800

    设置集群运行模式

--- storm-demo-back/src/main/java/com/lvtu/monitor/MainRoutine.java
+++ storm-demo-back/src/main/java/com/lvtu/monitor/MainRoutine.java
@@ -1,17 +1,25 @@
 package com.lvtu.monitor;
 
 import backtype.storm.LocalCluster;
+import backtype.storm.StormSubmitter;
+import backtype.storm.generated.AlreadyAliveException;
+import backtype.storm.generated.InvalidTopologyException;
 
 import com.lvtu.monitor.storm.topology.BehaviorTopology;
 import com.lvtu.monitor.utils.InitHelper;
 
 public class MainRoutine {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws AlreadyAliveException, InvalidTopologyException {
 		InitHelper.init();
-		LocalCluster cluster = new LocalCluster();
 		BehaviorTopology topology = new BehaviorTopology();
-		cluster.submitTopology(topology.getName(), topology.getConfig(),
-				topology.getTopology());
+		if (args.length == 0) {
+			LocalCluster cluster = new LocalCluster();
+			cluster.submitTopology(topology.getName(), topology.getConfig(),
+					topology.getTopology());
+		} else {
+			StormSubmitter.submitTopology(topology.getName(),
+					topology.getConfig(), topology.getTopology());
+		}
 	}
 }
